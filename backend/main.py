b20528123a1c9beaaf3f11a6d12d7c697aea835f
Prudvi Kuamar Reddy NS <22102A030231@mbu.asia>
2025-03-22 20:48:56 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -23,7 +23,7 @@ app = FastAPI(
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # In production, replace with specific origins
+    allow_origins=["https://remote-health-monitoring-chatbot-using-gemini.vercel.app/", "https://remote-health-monitoring-chatb-git-c3822c-vemu-project-b4a5aba8.vercel.app/", "https://remote-health-monitoring-chatbot-using-gemini-a6bq8davj.vercel.app/"],  # In production, replace with specific origins
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
